71dff7e58a4b5881fd55d2ec4fefee7d8b0950b2
Lcilento <166465345+CilentoX@users.noreply.github.com>
2024-10-09 14:26:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Escuta em todos os endereços IP
+    port: 5000, // Ou a porta que você está utilizando
+  },
 });
